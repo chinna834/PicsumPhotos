d63e2622d1050445e11ee09de75938a1ef75f3d6
Chinna Addepally <chinna834@gmail.com>
2021-05-02 20:23:41 -0500

    BaseProjectCreated

---
+++ PicsumPhotos/PicsumPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicsumPhotos
+//
+//  Created by Chinna Addepally on 5/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
